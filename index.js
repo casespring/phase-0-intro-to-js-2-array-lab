61fa3f1dea41a185883378329848a59d075bf699
Marco Upia <markremembers@gmail.com>
2023-10-14 18:57:57 -0400

    Slicing and Spreadinggit add . I'm a fool

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+};
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+};
+
+function appendCat(name) {
+    return [...cats, name];
+};
+
+function prependCat(name) {
+    return [name, ...cats];
+};
+
+function removeLastCat() {
+    return [...cats.slice(0, 2)]
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1)
+}
